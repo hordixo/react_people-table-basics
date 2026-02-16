d17eb61bfc072629876c8d9d9ec4c225a5977bd7
Oleksandr Hordiienko <g.aleks2308@gmail.com>
2026-02-16 16:50:56 +0200

    change solution

--- src/components/Loader/PeoplePage/PeoplePage.tsx
+++ src/components/Loader/PeoplePage/PeoplePage.tsx
@@ -2,13 +2,13 @@ import React, { useEffect, useState } from 'react';
 import { Loader } from '../Loader';
 import { Person } from '../../../types';
 import { getPeople } from '../../../api';
-import { PersonLink } from '../PersonLink';
 import { useParams } from 'react-router-dom';
+import { PeopleTable } from '../PeopleTable';
 
 export const PeoplePage: React.FC = () => {
   const [peoples, setPeoples] = useState<Person[]>([]);
   const [isLoading, setIsLoading] = useState(true);
-  const [errorMassage, setErrorMessage] = useState<string | null>(null);
+  const [errorMessage, setErrorMessage] = useState<string | null>(null);
   const { slug } = useParams();
 
   useEffect(() => {
@@ -27,7 +27,7 @@ export const PeoplePage: React.FC = () => {
             <Loader />
           ) : (
             <>
-              {errorMassage && (
+              {errorMessage && (
                 <p data-cy="peopleLoadingError" className="has-text-danger">
                   Something went wrong
                 </p>
@@ -38,73 +38,7 @@ export const PeoplePage: React.FC = () => {
                   There are no people on the server
                 </p>
               )}
-              {!isLoading && (
-                <table
-                  data-cy="peopleTable"
-                  className="
-                  table is-striped is-hoverable is-narrow is-fullwidth
-                  "
-                >
-                  <thead>
-                    <tr>
-                      <th>Name</th>
-                      <th>Sex</th>
-                      <th>Born</th>
-                      <th>Died</th>
-                      <th>Mother</th>
-                      <th>Father</th>
-                    </tr>
-                  </thead>
-
-                  <tbody>
-                    {peoples.map(people => {
-                      const mother = peoples.find(
-                        p => p.name === people.motherName,
-                      );
-
-                      const father = peoples.find(
-                        p => p.name === people.fatherName,
-                      );
-
-                      return (
-                        <tr
-                          data-cy="person"
-                          key={people.slug}
-                          className={
-                            slug === people.slug ? 'has-background-warning' : ''
-                          }
-                        >
-                          <td>
-                            <PersonLink person={people} />
-                          </td>
-
-                          <td>{people.sex}</td>
-                          <td>{people.born}</td>
-                          <td>{people.died}</td>
-                          <td>
-                            {mother ? (
-                              <PersonLink person={mother} />
-                            ) : people.motherName ? (
-                              people.motherName
-                            ) : (
-                              '-'
-                            )}
-                          </td>
-                          <td>
-                            {father ? (
-                              <PersonLink person={father} />
-                            ) : people.fatherName ? (
-                              people.fatherName
-                            ) : (
-                              '-'
-                            )}
-                          </td>
-                        </tr>
-                      );
-                    })}
-                  </tbody>
-                </table>
-              )}
+              <PeopleTable peoples={peoples} slug={slug} />
             </>
           )}
         </div>
